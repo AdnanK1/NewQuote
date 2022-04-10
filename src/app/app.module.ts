922ec937535a863e1e22054496930d79a08fd960
AdnanK1 <adnangitonga@gmail.com>
2022-04-10 15:54:09 +0300

    Generated the component form/form1 .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Form1Component } from './form/form1/form1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Form1Component
   ],
   imports: [
     BrowserModule,
