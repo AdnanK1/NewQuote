23833906ce7ee33d20b694888c7171edd917653f
AdnanK1 <adnangitonga@gmail.com>
2022-04-10 17:22:13 +0300

    Generate component index.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Form1Component } from './form/form1/form1.component';
+import { IndexComponent } from './form/index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Form1Component
+    Form1Component,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
