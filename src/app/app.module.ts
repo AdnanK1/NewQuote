4f597d93288f36ee9ba00bb1fe414c37e32822af
AdnanK1 <adnangitonga@gmail.com>
2022-04-10 17:46:40 +0300

    Imported FormsModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Form1Component } from './form/form1/form1.component';
 import { IndexComponent } from './form/index/index.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +14,8 @@ import { IndexComponent } from './form/index/index.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
